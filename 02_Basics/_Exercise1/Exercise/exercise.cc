8c52e108799d7001a43a38f02ae0520582a03639
sunnuntai <gitty@yoal.li>
2024-05-14 08:11:55 +0000

    exercise done

--- 02_Basics/_Exercise1/Exercise/exercise.cc
+++ 02_Basics/_Exercise1/Exercise/exercise.cc
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <cstdint>
 
 #include "exercise.h"
 
 void mod_cross_sum(int I, int J)
 {
+
+    for(std::size_t i = 0; i < I; i++){
+        for (std::size_t j = 0; j < J; j++){
+            std::cout << "i: " << i << " , j:" << j << " := " << (((i+j) % 2)?"Ungerade!":"Gerade!") << std::endl;
+        }
+    }
+
 }
--- 02_Basics/_Exercise2/Exercise/main.cc
+++ 02_Basics/_Exercise2/Exercise/main.cc
@@ -1,14 +1,34 @@
 #include <cstdint>
 #include <iostream>
 
-void super_duper_example()
+void super_duper_example(const std::uint16_t input)
 {
+    static std::uint16_t counter = 0;
+    static constexpr std::uint16_t resetter = 10;
+
+    if (input == resetter){
+        counter = 0;
+    }
+    else if (input % 2 == 0){
+
+        counter++;
+
+    }
+
+    std::cout << counter << std::endl;
 
 }
 
 int main()
 {
+    std::uint16_t input = 0;
 
+    while (true){
+    std::cout << "Please input an Integer: ";
+    std::cin >>  input;
+
+    super_duper_example(input);
+    }
 
     return 0;
 }
