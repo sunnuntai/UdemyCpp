4da7a830793d57b7ae4dc4a2645573dc97a6d35f
sunnuntai <gitty@yoal.li>
2024-05-14 12:52:48 +0000

    Excercise done

--- 03_ArrayPointer/_Exercise1/Exercise/main.cc
+++ 03_ArrayPointer/_Exercise1/Exercise/main.cc
@@ -5,27 +5,28 @@
 
 int main()
 {
-    // Exercise 1
     constexpr auto array_length = std::size_t{100};
-    double array1[array_length] = {};
 
+    // Exercise 1
+    double array1[array_length] = {};
     for (std::size_t i = 0; i < array_length; i++)
     {
-        array1[i] = i;
+        array1[i] = static_cast<double>(i);
     }
 
     // Exercise 2
-    auto sum = 0.0;
-
     const auto sum = array_sum(array1, array_length);
     std::cout << "(Exercise 2) Array Sum = " << sum << '\n';
 
     // Exercise 3
     std::array<double, 100> array2;
-    auto sum2 = 0.0;
+    for (std::size_t i = 0; i < array_length; i++)
+    {
+        array2[i] = static_cast<double>(i);
+    }
 
     const auto sum2 = array_sum(array2);
-    std::cout << "(Exercise 2) Array Sum = " << sum << '\n';
+    std::cout << "(Exercise 3) Array Sum = " << sum2 << '\n';
 
     return 0;
 }
